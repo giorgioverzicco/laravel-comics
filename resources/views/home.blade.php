755231accd2ec0048fab4728bc17ebd33c30b320
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-07 16:52:58 +0200

    add simple home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,18 @@
 @endsection
 
 @section('page-content')
-    <div>
-        @foreach($comics as $comic)
-            <p>{{$comic['title']}}</p>
-        @endforeach
+    <div class="home-page">
+        <div class="container">
+            <div class="comics">
+                <div class="comics__grid">
+                    @foreach($comics as $comic)
+                        <a class="comic-card" href="/">
+                            <img class="comic-card__thumbnail" src="{{ $comic['thumb'] }}" alt="" />
+                            <span class="comic-card__title">{{ $comic['title'] }}</span>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
