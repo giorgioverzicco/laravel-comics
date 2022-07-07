70dd8f97d50c2f020c7dd3eb1cd449cb2f14a472
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-07 16:19:59 +0200

    add header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,49 @@
 <header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="DC Comics Logo">
+        </div>
 
+        <div class="main-nav">
+            <ul class="main-nav__list">
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Characters</a>
+                </li>
+                <li class="main-nav__item main-nav__item--active">
+                    <a href="/" class="main-nav__link">Comics</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Movies</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Tv</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Games</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Collectibles</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Videos</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Fans</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">News</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Shop</a>
+                </li>
+            </ul>
+        </div>
+
+        <form class="search-form">
+            <input class="search-form__input-text" type="text" placeholder="Search" />
+            <div class="search-form__icon">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </form>
+    </div>
 </header>
