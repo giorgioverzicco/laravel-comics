99735e7283735b4704f05471eddb350014c9ade6
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-07 17:00:42 +0200

    moved header and footer to partials

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,20 +1,20 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>
-            @yield('page-title')
-        </title>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    </head>
-    <body>
-        @include('layouts.header')
-        <main>
-            @yield('page-content')
-        </main>
-        @include('layouts.footer')
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        @yield('page-title')
+    </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+@include('partials.header')
+<main>
+    @yield('page-content')
+</main>
+@include('partials.footer')
+</body>
 </html>
