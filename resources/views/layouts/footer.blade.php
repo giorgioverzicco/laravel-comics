802ced73870b57374ca405ec3a187193f5c7cf3a
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-07 16:52:43 +0200

    add footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,2 +1,104 @@
 <footer>
+    <div class="container">
+        <section class="footer__section">
+            <h3 class="footer__section-title">DC Comics</h3>
+            <ul class="footer__section-list">
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Characters</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Comics</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Movies</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">TV</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Games</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Videos</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">News</a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="footer__section">
+            <h3 class="footer__section-title">Shop</h3>
+            <ul class="footer__section-list">
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Shop DC</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Shop DC Collectibles</a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="footer__section">
+            <h3 class="footer__section-title">DC</h3>
+            <ul class="footer__section-list">
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Terms Of Use</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Privacy policy (New)</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Ad Choices</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Advertising</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Jobs</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Subscriptions</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Talent Workshops</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">CPSC Certificates</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Ratings</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Shop Help</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">Contact Us</a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="footer__section">
+            <h3 class="footer__section-title">Sites</h3>
+            <ul class="footer__section-list">
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">DC</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">MAD Magazine</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">DC Kids</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">DC Universe</a>
+                </li>
+                <li class="footer__section-item">
+                    <a href="/" class="footer__section-link">DC Power Visa</a>
+                </li>
+            </ul>
+        </section>
+
+        <img src="{{asset('img/dc-logo-bg.png')}}" alt="" />
+    </div>
 </footer>
